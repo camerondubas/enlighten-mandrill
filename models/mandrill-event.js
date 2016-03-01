9bf23d58725f6515209c93ba64899d8768d8b282
Cameron Dubas <camerondubas@gmail.com>
2016-03-01 12:32:49 -0800

    Make Mandrill Events a class with the available Types

--- models/mandrill-event.js
+++ models/mandrill-event.js
@@ -1,118 +1,139 @@
 'use strict';
 
-let MandrillEventTypes = {
-  "hard_bounce": {
+var stringOperations = require('../utils/string-operations');
+
+class MandrillEvent {
+  constructor(type, message) {
+    this.message = this[
+      stringOperations.toCamelCase(type) || 'default'
+    ](message);
+  }
+
+  hardBounce(message) {
+    return {
       username: "An Email Just Hard-Bounced",
       text: '',
       icon_emoji: ":email:",
       attachments: [
         {
-          "fallback": `It was sent to ${event.msg.email}`,
+          "fallback": `It was sent to ${message.email}`,
           "color": "danger",
           "fields": [
             {
               "title": 'Sent To',
-              "value": event.msg.email,
+              "value": message.email,
               "short": true,
             },
             {
               "title": 'Subject',
-              "value": event.msg.subject,
+              "value": message.subject,
               "short": true,
             },
             {
               "title": 'Error Description',
-              "value": event.msg.diag || "No description provided.",
+              "value": message.diag || "No description provided.",
               "short": false,
             }
           ]
         }
       ]
-    },
-  "soft_bounce": {
+    };
+  }
+
+  softBounce(message) {
+    return {
       username: "An Email Just Soft-Bounced",
       text: '',
       icon_emoji: ":email:",
       attachments: [
         {
-          "fallback": `It was sent to ${event.msg.email}`,
-          "color": "danger",
+          "fallback": `It was sent to ${message.email}`,
+          "color": "warning",
           "fields": [
             {
               "title": 'Sent To',
-              "value": event.msg.email,
+              "value": message.email,
               "short": true,
             },
             {
               "title": 'Subject',
-              "value": event.msg.subject,
+              "value": message.subject,
               "short": true,
             },
             {
               "title": 'Error Description',
-              "value": event.msg.diag || "No description provided.",
+              "value": message.diag || "No description provided.",
               "short": false,
             }
           ]
         }
       ]
-    },
-  "reject": {
-     username: "An Email Was Just Rejected",
-      text: '',
-      icon_emoji: ":email:",
-      attachments: [
-        {
-          "fallback": `It was sent to ${event.msg.email}`,
-          "color": "danger",
-          "fields": [
-            {
-              "title": 'Sent To',
-              "value": event.msg.email,
-              "short": true,
-            },
-            {
-              "title": 'Subject',
-              "value": event.msg.subject,
-              "short": true,
-            },
-            {
-              "title": 'Error Description',
-              "value": event.msg.diag || "No description provided.",
-              "short": false,
-            }
-          ]
-        }
-      ]
-  },
-  "default": {
-     username: "An Email Just Had An Issue",
-      text: '',
-      icon_emoji: ":email:",
-      attachments: [
-        {
-          "fallback": `It was sent to ${event.msg.email}`,
-          "color": "danger",
-          "fields": [
-            {
-              "title": 'Sent To',
-              "value": event.msg.email,
-              "short": true,
-            },
-            {
-              "title": 'Subject',
-              "value": event.msg.subject,
-              "short": true,
-            },
-            {
-              "title": 'Error Description',
-              "value": event.msg.diag || "No description provided.",
-              "short": false,
-            }
-          ]
-        }
-      ]
+    };
   }
+
+  reject(message) {
+    return {
+      username: "An Email Was Just Rejected",
+      text: '',
+      icon_emoji: ":email:",
+      attachments: [
+        {
+          "fallback": `It was sent to ${message.email}`,
+          "color": "danger",
+          "fields": [
+            {
+              "title": 'Sent To',
+              "value": message.email,
+              "short": true,
+            },
+            {
+              "title": 'Subject',
+              "value": message.subject,
+              "short": true,
+            },
+            {
+              "title": 'Error Description',
+              "value": message.diag || "No description provided.",
+              "short": false,
+            }
+          ]
+        }
+      ]
+    };
+  }
+
+  default(message) {
+    return {
+      username: "An Email Just Had An Issue",
+      text: '',
+      icon_emoji: ":email:",
+      attachments: [
+        {
+          "fallback": `It was sent to ${message.email}`,
+          "color": "warning",
+          "fields": [
+            {
+              "title": 'Sent To',
+              "value": message.email,
+              "short": true,
+            },
+            {
+              "title": 'Subject',
+              "value": message.subject,
+              "short": true,
+            },
+            {
+              "title": 'Error Description',
+              "value": message.diag || "No description provided.",
+              "short": false,
+            }
+          ]
+        }
+      ]
+    };
+  }
+
 };
 
-module.exports = MandrillEventTypes;
+
+module.exports = MandrillEvent;
--- routes/webhooks.js
+++ routes/webhooks.js
@@ -3,54 +3,26 @@
 var express             = require('express');
 var router              = express.Router();
 var SlackMessage        = require('../models/slack-message');
-var mandrillEventTypes  = require('../utils/mandrill-event-types');
+var MandrillEvent       = require('../models/mandrill-event');
 
 router.post('/mandrill', function(req, res, next) {
   if (!req.get('X-Mandrill-Signature') || req.get('X-Mandrill-Signature') !== process.env.MANDRILL_SIGNATURE) {
     var e = new Error();
     e.status = 401;
-    next(e);
+    return next(e);
   }
 if (req.body.mandrill_events) {
   var events = JSON.parse(req.body.mandrill_events);
 
   events.forEach(event => {
-    let message = {
-      username: mandrillEventTypes[event.event] || mandrillEventTypes['default'],
-      text: '',
-      icon_emoji: ":email:",
-      attachments: [
-        {
-          "fallback": `It was sent to ${event.msg.email}`,
-          "color": "danger",
-          "fields": [
-            {
-              "title": 'Sent To',
-              "value": event.msg.email,
-              "short": true,
-            },
-            {
-              "title": 'Subject',
-              "value": event.msg.subject,
-              "short": true,
-            },
-            {
-              "title": 'Error Description',
-              "value": event.msg.diag || "No description provided.",
-              "short": false,
-            }
-          ],
+    var mandrillEvent = new MandrillEvent(event.event, event.msg);
+    var slackMessage = new SlackMessage(mandrillEvent.message);
 
-        }
-
-      ]
-    };
-
-    new SlackMessage(message).sendWebhookMessage(process.env.SLACK_URL);
+    slackMessage.sendWebhookMessage(process.env.SLACK_URL);
   }, this);
 }
 
- next(req.body);
+ return next(req.body);
 });
 
 module.exports = router;
